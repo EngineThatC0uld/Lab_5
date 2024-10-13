894bea8cfb3ce2fd206aeea0ea8d6849396041a4
EngineThatC0uld <pepeisdet@gmail.com>
2024-10-13 18:44:38 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -7,7 +7,7 @@ function Calculator(){
     const price = parseInt(selected_product.value);
     const number = parseInt(selected_quantity.value);
     const total = price * number;
-    if(number < 0 || number == NaN)
+    if(number < 0 || isNaN(number))
         result.textContent = 'Введите корректное число продуктов!';
     else 
         result.textContent = 'Стоимость: ' + total.toString() + ' рублей';
